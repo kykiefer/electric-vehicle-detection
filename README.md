1d49dbb848984f8ebd5d55bcf2c35bf26168da24
Ky Kiefer <kkiefer@umich.edu>
2017-07-01 13:17:31 -0600

    cleaning up

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # Electric Vehicle Detection
 Gridcure predictive modeling challenge
 
-Work in progress (6/28/2017)...
+Work in progress (7/1/2017)...
 
 1. Which residences have electric vehicles?
-I threw together a quick Random Forest on 4 engineered features (min power, max power, (max - min power), avg power) and performed a grid search to establish a baseline. I chose to optimize the model parameter based on f1 score as accuracy is not appropriate for severely unbalanced classes (AUC score would have also been appropriate).
 
-With a bit of tuning, my best Random Forest had an `F1 score` of `0.715` and an `accuracy` of `84%` (a 21% improvement over guessing in proportion to the known number of EVs) using 3-fold cross-validation. This is what I will use as my baseline comparative model for predicting which residences have EVs. I suspect a bit of feature engineering and perhaps applying Gradient Boosting will go a long way.
+I threw together a quick Random Forest on just 2 engineered features (average power and power range (max - min)) and performed a grid search to establish a baseline. I chose to optimize the model parameter based on F1 score, as accuracy is not appropriate for severely unbalanced classes (AUC score would have also been appropriate).
+
+My best Random Forest had an `F1 score` of `0.684` and an `accuracy` of `81%` (a 17% improvement over guessing in proportion to the known number of EVs) using 3-fold cross-validation. This is what I will use as my baseline comparative model for predicting which residences have EVs. I suspect a bit of feature engineering and applying Gradient Boosting will go a long way.
 
 To be continued...
--- gridcure.py
+++ gridcure.py
@@ -5,72 +5,88 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import cross_val_score
 np.random.seed(15)  # for reproducibility
 
-# load developement data
-df = pd.read_csv('EV_files/EV_train.csv')
-labels = pd.read_csv('EV_files/EV_train_labels.csv')
 
-# set index as House ID
-df.set_index('House ID', inplace=True)
-labels.set_index('House ID', inplace=True)
+def load_data():
+    # load developement data
+    df = pd.read_csv('EV_files/EV_train.csv')
+    labels = pd.read_csv('EV_files/EV_train_labels.csv')
 
-# get has_EV label for household
-df['has_EV'] = labels.max(axis=1)
+    # set index as House ID
+    df.set_index('House ID', inplace=True)
+    labels.set_index('House ID', inplace=True)
 
-# create avg, stdev, min,  max, range and other values per house
-df['Pwr_Min'] = df.max(axis=1)
-df['Pwr_Max'] = df.max(axis=1)
-df['Pwr_Avg'] = df.mean(axis=1)
-df['Pwr_Rng'] = df.max(axis=1) - df.min(axis=1)
-df['Pwr_Std'] = df.std(axis=1)
-df['Pwr_IQR'] = df.quantile(q=0.75, axis=1) - df.quantile(q=0.25, axis=1)
+    # create series with index as House ID and has_ev label (0/1)
+    ev_label = labels.max(axis=1).rename('has_ev')
 
-# select features to fit model on
-y = df.pop('has_EV')
-features = ['Pwr_Min', 'Pwr_Max', 'Pwr_Avg', 'Pwr_Rng']
-X = df[features]
-
-# # grid parameters
-# param_grid = {'n_estimators': [15, 20, 25],
-#               'max_depth': [12, 15, 18, None],
-#               'max_features': ['auto', 'log2'],
-#               'min_samples_split': [2, 3, 4],
-#               'min_samples_leaf': [1, 2],
-#               'bootstrap': [True, False],
-#               'criterion': ['gini', 'entropy']}
-#
-# # tune for f1 score to establish baseline
-# scores = ['f1']
-#
-# for score in scores:
-#     print('Tuning hyper-parameters for {}'.format(score))
-#
-#     rf = GridSearchCV(RandomForestClassifier(n_jobs=-1), param_grid=param_grid, cv=3, scoring=score)
-#     rf.fit(X, y)
-#
-#     print('Best parameters set found on development set: {}'.format(rf.best_params_))
-#     print()
-#     print('Grid scores on development set:')
-#     print()
-#     means = rf.cv_results_['mean_test_score']
-#     stds = rf.cv_results_['std_test_score']
-#     for mean, std, params in zip(means, stds, rf.cv_results_['params']):
-#         print('%0.3f (+/-%0.03f) for %r'
-#               % (mean, std * 2, params))
-
-# from the grid search these are our best model hyper-parameters
-best_rf = RandomForestClassifier(n_estimators=25, criterion='entropy',
-                                 max_features='log2', max_depth=None,
-                                 min_samples_leaf=1, min_samples_split=2,
-                                 bootstrap=True, n_jobs=-1)
-best_rf.fit(X, y)
-print('features:  ', features)
-print('importance:', best_rf.feature_importances_)
-scores = cross_val_score(best_rf, X, y, cv=3)
-print('Accuracy: %0.2f (+/- %0.2f)' % (scores.mean(), scores.std() * 2))
+    return df, ev_label
 
 
+def feature_engineer(df, ev_label):
+    # create avg, stdev, min,  max, range and other values per house
+    df['Pwr_Min'] = df.max(axis=1)
+    df['Pwr_Max'] = df.max(axis=1)
+    df['Pwr_Avg'] = df.mean(axis=1)
+    df['Pwr_Rng'] = df.max(axis=1) - df.min(axis=1)
+    df['Pwr_Std'] = df.std(axis=1)
+    df['Pwr_IQR'] = df.quantile(q=0.75, axis=1) - df.quantile(q=0.25, axis=1)
 
-# # fill missing intervals with house mean
-# # don't love this approach but good for first pass
-# house_mean = pd.DataFrame({col: X.mean(axis=1) for col in X.columns})
-# X.fillna(house_mean, inplace=True)
+    # inner join on House ID index
+    df = pd.concat((df, ev_label), axis=1, join='inner')
+
+    # select features to fit model on
+    y = df.pop('has_ev')
+    return df, y
+
+
+def rf_grid(X, y):
+    # grid parameters
+    param_grid = {'n_estimators': [15, 20, 25],
+                  'max_depth': [12, 15, 18, None],
+                  'max_features': ['auto', 'log2'],
+                  'min_samples_split': [2, 3, 4],
+                  'min_samples_leaf': [1, 2],
+                  'bootstrap': [True, False],
+                  'criterion': ['gini', 'entropy']}
+
+    # tune for f1 score to establish baseline
+    scores = ['f1']
+
+    for score in scores:
+        print('Tuning hyper-parameters for {}'.format(score))
+
+        rf = GridSearchCV(RandomForestClassifier(n_jobs=-1), param_grid=param_grid, cv=3, scoring=score)
+        rf.fit(X, y)
+
+        print('Best parameters set found on development set: {}'.format(rf.best_params_))
+        print()
+        print('Grid scores on development set:')
+        print()
+        means = rf.cv_results_['mean_test_score']
+        stds = rf.cv_results_['std_test_score']
+        for mean, std, params in zip(means, stds, rf.cv_results_['params']):
+            print('%0.3f (+/-%0.03f) for %r'
+                  % (mean, std * 2, params))
+        return rf
+
+
+if __name__ == '__main__':
+    df, ev_label = load_data()
+    X, y = feature_engineer(df, ev_label)
+
+    # random forest features
+    features = ['Pwr_Avg', 'Pwr_Rng']
+    X = df[features]
+
+    # print('Performing grid search...')
+    # rf = rf_grid(X, y)
+
+    # from the f1 sscore grid search these are our best model hyper-parameters
+    best_rf = RandomForestClassifier(n_estimators=15, criterion='entropy',
+                                     max_features='log2', max_depth=12,
+                                     min_samples_leaf=2, min_samples_split=2,
+                                     bootstrap=True, n_jobs=-1)
+
+    scores = cross_val_score(best_rf, X, y, cv=3)
+    print('Accuracy: %0.2f (+/- %0.2f)' % (scores.mean(), scores.std() * 2))
+    best_rf.fit(X, y)
+    print('feature importance:', best_rf.feature_importances_)
