14bff88a2d40ef4ee9ea592cd9073e26264aaa09
Ky Kiefer <kkiefer@umich.edu>
2017-07-04 22:57:13 -0600

    added boosted tree

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
 # Electric Vehicle Detection
 Gridcure predictive modeling challenge
 
-Work in progress (7/1/2017)...
+Work in progress (7/4/2017)...
 
 1. Which residences have electric vehicles?
 
-I threw together a quick Random Forest on just 2 engineered features (average power and power range (max - min)) and performed a grid search to establish a baseline. I chose to optimize the model parameter based on F1 score, as accuracy is not appropriate for severely unbalanced classes (AUC score would have also been appropriate).
+I threw together a quick Random Forest on just 3 engineered features (`Pwr_Rng`, `Pwr_Std`, `Avg_Norm_Rng`) and performed a grid search to establish a baseline. I chose to optimize the model parameters based on F1 score, as accuracy is not appropriate for imbalanced classes.
 
-My best Random Forest had an `F1 score` of `0.684` and an `accuracy` of `81%` (a 17% improvement over guessing in proportion to the known number of EVs) using 3-fold cross-validation. This is what I will use as my baseline comparative model for predicting which residences have EVs. I suspect a bit of feature engineering and applying Gradient Boosting will go a long way.
+My best Random Forest had an `F1 score` of `0.686` and an `accuracy` using 3-fold cross-validation. This is what I will use as my baseline comparative model for predicting which residences have EVs.
+
+I next wanted to try gradient boosting. An out of the box gradient boosted classifier (using xgboost) yielded an `F1 score` of `0.716`. Very promising, considering boosted models typically yield good improvements with detailed tuning.
+
+Next step is to perform a quick grid search on the xgboost model, but I ultimately expect going back for some additional feature engineering to be most critical.
 
 To be continued...
--- gridcure.py
+++ gridcure.py
@@ -3,6 +3,7 @@ import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import cross_val_score
+from xgboost import XGBClassifier
 np.random.seed(15)  # for reproducibility
 
 
@@ -15,27 +16,28 @@ def load_data():
     df.set_index('House ID', inplace=True)
     labels.set_index('House ID', inplace=True)
 
-    # create series with index as House ID and has_ev label (0/1)
-    ev_label = labels.max(axis=1).rename('has_ev')
+    # create series with index as House ID and Has_EV label (0/1)
+    ev_label = labels.max(axis=1).rename('Has_EV')
 
     return df, ev_label
 
 
 def feature_engineer(df, ev_label):
     # create avg, stdev, min,  max, range and other values per house
-    df['Pwr_Min'] = df.max(axis=1)
-    df['Pwr_Max'] = df.max(axis=1)
-    df['Pwr_Avg'] = df.mean(axis=1)
-    df['Pwr_Rng'] = df.max(axis=1) - df.min(axis=1)
-    df['Pwr_Std'] = df.std(axis=1)
-    df['Pwr_IQR'] = df.quantile(q=0.75, axis=1) - df.quantile(q=0.25, axis=1)
+    X = df.mean(axis=1).rename('Pwr_Avg').to_frame()
+    X['Pwr_Med'] = df.median(axis=1)
+    X['Pwr_Rng'] = df.max(axis=1) - df.min(axis=1)
+    X['Pwr_Std'] = df.std(axis=1)
+    X['Pwr_IQR'] = df.quantile(q=0.75, axis=1) - df.quantile(q=0.25, axis=1)
+    X['Avg_Norm_Rng'] = X['Pwr_Rng'] / X['Pwr_Avg']
+    X['IQR_Norm'] = X['Pwr_IQR'] / X['Pwr_Avg']
 
     # inner join on House ID index
-    df = pd.concat((df, ev_label), axis=1, join='inner')
+    X = pd.concat((X, ev_label), axis=1, join='inner')
 
     # select features to fit model on
-    y = df.pop('has_ev')
-    return df, y
+    y = X.pop('Has_EV')
+    return X, y
 
 
 def rf_grid(X, y):
@@ -64,29 +66,38 @@ def rf_grid(X, y):
         means = rf.cv_results_['mean_test_score']
         stds = rf.cv_results_['std_test_score']
         for mean, std, params in zip(means, stds, rf.cv_results_['params']):
-            print('%0.3f (+/-%0.03f) for %r'
-                  % (mean, std * 2, params))
+            print('%0.3f (+/-%0.03f) accuracy for %r' % (mean, std * 2, params))
         return rf
+
+def boosted_trees(X, y):
+    pass
 
 
 if __name__ == '__main__':
     df, ev_label = load_data()
     X, y = feature_engineer(df, ev_label)
 
+    # # gradient boosted features on selected features
+    # features = ['Pwr_Avg', 'Pwr_Rng', 'Normed_Pwr_Rng']
+    # X = df[features]
+    # boosted_tree = XGBClassifier(max_depth=2)
+    # scores = cross_val_score(boosted_tree, X, y, cv=3, scoring='f1')
+    # print('F1', scores.mean())
+
     # random forest features
-    features = ['Pwr_Avg', 'Pwr_Rng']
-    X = df[features]
+    features = ['Pwr_Rng', 'Pwr_Std', 'Avg_Norm_Rng']
+    X = X[features]
 
-    # print('Performing grid search...')
-    # rf = rf_grid(X, y)
+    print('Performing grid search...')
+    rf = rf_grid(X, y)
 
-    # from the f1 sscore grid search these are our best model hyper-parameters
-    best_rf = RandomForestClassifier(n_estimators=15, criterion='entropy',
-                                     max_features='log2', max_depth=12,
-                                     min_samples_leaf=2, min_samples_split=2,
-                                     bootstrap=True, n_jobs=-1)
-
-    scores = cross_val_score(best_rf, X, y, cv=3)
-    print('Accuracy: %0.2f (+/- %0.2f)' % (scores.mean(), scores.std() * 2))
-    best_rf.fit(X, y)
-    print('feature importance:', best_rf.feature_importances_)
+    # # from the f1 sscore grid search these are our best model hyper-parameters
+    # best_rf = RandomForestClassifier(n_estimators=15, criterion='entropy',
+    #                                  max_features='log2', max_depth=12,
+    #                                  min_samples_leaf=2, min_samples_split=2,
+    #                                  bootstrap=True, n_jobs=-1)
+    #
+    # scores = cross_val_score(best_rf, X, y, cv=3)
+    # print('Accuracy: %0.2f (+/- %0.2f)' % (scores.mean(), scores.std() * 2))
+    # best_rf.fit(X, y)
+    # print('feature importance:', best_rf.feature_importances_)
